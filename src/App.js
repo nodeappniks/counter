1a1e3935a354b8ecfd176e7f84e18840821e84a7
Nikhil Kumar <nikhilkumar@Nikhils-MacBook-Pro.local>
2022-10-22 19:52:25 +0530

    added login functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Counter from "./Components/Counter";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
+import Login from "./Pages/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
 //css
 const styles = createUseStyles({
   heading: {
@@ -21,9 +23,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/home" element={<Home />}></Route>
+        <Route exact path="/" element={<Home />}></Route>
         <Route exact path="/about" element={<About />}></Route>
-        <Route exact path="/" element={<Counter styleCss={css} />}></Route>
+        <Route exact path="/login" element={<Login />}></Route>
+        {/* <Route exact path="/" element={<Counter styleCss={css} />}></Route> */}
       </Routes>
     </Router>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  let navigate = useNavigate();
+
   return (
     <>
-      <Link to="/">Go Back</Link>
+      <div onClick={() => navigate(-1)}>Go Back</div>
       <div>I am in Home Page</div>
     </>
   );
